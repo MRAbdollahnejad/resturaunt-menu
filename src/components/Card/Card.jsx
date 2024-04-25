65f40e5d55d9fd6dbd5c0ff751fe3c76a47f329b
mrabd <reza123.reza987@gmail.com>
2024-04-25 19:03:05 +0330

    initial react  project

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card(){
+    return(
+        <div>
+          <div>
+            <div></div>
+            <div></div>
+            </div>  
+          <div></div>
+        </div>
+    )
+}
